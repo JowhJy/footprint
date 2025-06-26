8ba38454b4d04707c4a26104bd77086506fa24ca
JowhJy <johanwhoek@gmail.com>
2025-06-26 11:07:13 +0200

    1.21.6

--- src/main/java/com/jowhjy/mixin/ServerPlayerEntity_Mixin.java
+++ src/main/java/com/jowhjy/mixin/ServerPlayerEntity_Mixin.java
@@ -2,13 +2,11 @@ package com.jowhjy.mixin;
 
 import com.jowhjy.ChunkGetter;
 import com.jowhjy.config.FootprintConfigs;
-import com.jowhjy.mixin_interfaces.IChunkWithForcedSave;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntity_Mixin extends PlayerEntity {
-    public ServerPlayerEntity_Mixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-        super(world, pos, yaw, gameProfile);
+    public ServerPlayerEntity_Mixin(World world, GameProfile gameProfile) {
+        super(world, gameProfile);
     }
 
     @Inject(method = "dropItem", at = @At("HEAD"))
